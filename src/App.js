df8d3eb3ddf28f85c1eb0a8b1b361a75bb6149e6
nikurou <47164155+nikurou@users.noreply.github.com>
2021-07-25 09:18:30 -1000

    Populate Canvas with Donut and Stars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, Suspense, useEffect } from "react";
+import "./App.css";
+import { Canvas, useThree } from "@react-three/fiber";
 
-function App() {
+import Torus from "./components/Torus";
+import TestBox from "./components/TestBox";
+import Stars from "./components/Stars";
+
+import { useSpring, a } from "react-spring";
+import { OrbitControls } from "@react-three/drei";
+
+const App = () => {
+  const [posArray, setPos] = useState([]);
+
+  // Trigger on Initial Render
+  // Generate position array for Stars between -100 and 100
+  useEffect(() => {
+    const temp = [];
+    for (let i = 0; i < 700; i++) {
+      let ind = [
+        Math.floor(Math.random() * (100 - -100 + 1) + -100),
+        Math.floor(Math.random() * (100 - -100 + 1) + -100),
+        Math.floor(Math.random() * (100 - -100 + 1) + -100),
+      ];
+      temp.push(ind);
+    }
+    setPos(temp);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-scene">
+      <div className="donut-scene">
+        <Canvas colorManagement camera={{ position: [-5, 0, 0] }}>
+          <ambientLight intensity={0.2} />
+          <pointLight position={[20, 20, 20]} intensity={0.5} />
+
+          {/*Add 200 Random Stars*/}
+          {posArray.map((arr) => (
+            <Stars position={[arr[0], arr[1], arr[2]]}></Stars>
+          ))}
+
+          {/* Add the Donut */}
+          <Suspense fallback={null}>
+            <Torus />
+          </Suspense>
+
+          <OrbitControls />
+        </Canvas>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
