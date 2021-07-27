0da276680f27d395e217b8a96c46672f4a8153f9
nikurou <47164155+nikurou@users.noreply.github.com>
2021-07-26 21:56:44 -1000

    earth texture and component

---
+++ src/components/Earth.js
@@ -0,0 +1,27 @@
+import React, { Suspense, useRef } from "react";
+import { useFrame, useLoader, Canvas } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import earth_texture from "../assets/textures/earth_texture.jpg";
+
+const Earth = (props) => {
+  const mesh = useRef();
+  useFrame(() => {
+    mesh.current.rotation.x += 0;
+    mesh.current.rotation.y += 0.0005;
+    mesh.current.rotation.z += 0.0001;
+  });
+
+  //Earth TextureMap
+  const earthTexture = useLoader(TextureLoader, earth_texture);
+  //Earth NormalMap
+  // Might not need
+
+  return (
+    <mesh ref={mesh} scale={0.45} position={props.position}>
+      <sphereBufferGeometry attach="geometry" args={[30, 50, 50]} />
+      <meshStandardMaterial map={earthTexture} attach="material" />
+    </mesh>
+  );
+};
+
+export default Earth;
