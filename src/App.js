8b6ff7f3325fa0984cec9167f28cadecf8ebca2f
nikurou <47164155+nikurou@users.noreply.github.com>
2021-07-27 22:16:45 -1000

    Load in and change the bomb's color

--- src/App.js
+++ src/App.js
@@ -3,30 +3,22 @@ import "./App.css";
 import { Canvas, useThree, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Html, useProgress } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 
+//Components
 import Torus from "./components/Torus";
 import TestBox from "./components/TestBox";
 import Star from "./components/Stars";
-
-import { OrbitControls, Stars } from "@react-three/drei";
 import Earth from "./components/Earth";
 import Moon from "./components/Moon";
+
+// 3D Models
 import bomb from "./assets/3d_models/bomb.gltf";
+import headPhone from "./assets/3d_models/headphone_model/headphone.gltf";
 
 const App = () => {
-  // Star Position Array
+  // Star Position Array Hook
   const [posArray, setPos] = useState([]);
-  // Temporary Headphone Model
-
-  const Model = () => {
-    const gltf = useLoader(GLTFLoader, bomb);
-    return <primitive object={gltf.scene} scale={0.4} />;
-  };
-
-  const Loader = () => {
-    const { active, progress, errors, item, loaded, total } = useProgress();
-    return <Html center>{progress} % loaded</Html>;
-  };
 
   // Trigger on Initial Render
   // Generate position array for Stars between -100 and 100
@@ -43,8 +35,24 @@ const App = () => {
     setPos(temp);
   }, []);
 
+  // Temporary Bomb Model
+  const Model = () => {
+    const gltf = useLoader(GLTFLoader, bomb);
+    console.log(gltf.scene);
+    // Code to change color
+    gltf.scene.getObjectByName("Handles").material.color.set("blue");
+    return <primitive object={gltf.scene} scale={0.4} />;
+  };
+
+  // Suspense fallback Loader
+  const Loader = () => {
+    const { active, progress, errors, item, loaded, total } = useProgress();
+    return <Html center>{progress} % loaded</Html>;
+  };
+
   return (
     <div className="bg-scene">
+      <p>Hello</p>
       <div className="donut-scene">
         <Canvas colorManagement camera={{ position: [-5, 0, 0] }}>
           <ambientLight intensity={0.2} />
@@ -66,7 +74,7 @@ const App = () => {
             {/* Add the Moon */}
             <Moon position={[400, 50, -40]} />
             {/* Add Donut */}
-            <Torus />
+            {/* <Torus /> */}
           </Suspense>
 
           <OrbitControls />
