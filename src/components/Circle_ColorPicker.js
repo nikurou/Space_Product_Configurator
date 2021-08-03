eeabf1b9ec160a8dedc517559749f2c8123d3198
nikurou <47164155+nikurou@users.noreply.github.com>
2021-08-02 23:34:31 -1000

    Color Picker component

---
+++ src/components/Circle_ColorPicker.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { CirclePicker } from "react-color";
+
+const Circle_ColorPicker = (props) => {
+  const color_selection = [
+    "#D9E3F0",
+    "#F47373",
+    "#697689",
+    "#37D67A",
+    "#2CCCE4",
+    "#555555",
+    "#dce775",
+    "#ff8a65",
+    "#ba68c8",
+  ];
+
+  return (
+    <CirclePicker
+      width="100%"
+      color={color_selection}
+      onChange={(color) => {
+        props.handleColorChange(color);
+      }}
+    ></CirclePicker>
+  );
+};
+
+export default Circle_ColorPicker;
