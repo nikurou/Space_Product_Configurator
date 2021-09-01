80b5256eb1691e9e6d44cf3c78c5edec8ef0de39
nikurou <47164155+nikurou@users.noreply.github.com>
2021-08-31 22:43:15 -1000

    Suspense should not be in the component, removed

--- src/components/3D_Components/Fiat_Punto.js
+++ src/components/3D_Components/Fiat_Punto.js
@@ -6,7 +6,7 @@ source: https://sketchfab.com/3d-models/free-1995-fiat-punto-gt-48db6facb4b64e99
 title: (FREE) 1995 Fiat Punto GT
 */
 
-import React, { useRef, Suspense, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 
 const Fiat_Punto = (props) => {
@@ -25,52 +25,50 @@ const Fiat_Punto = (props) => {
   }, []);
 
   return (
-    <Suspense fallback={null}>
-      <group ref={group} {...props} dispose={null}>
-        <group rotation={[-Math.PI / 2, 0, 0]}>
-          <group position={[-0.04, 0.03, -0.04]} scale={0.46}>
-            <mesh
-              geometry={nodes.Punto_GT_0.geometry}
-              material={materials.gt_black}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_1.geometry}
-              material={materials.gt_license}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_2.geometry}
-              material={materials.gt_tire}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_3.geometry}
-              material={materials.gt_rim}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_4.geometry}
-              material={materials.gt_windows}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_5.geometry}
-              material={materials["Hemi.001"]}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_6.geometry}
-              material={materials.gt_body}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_7.geometry}
-              material={materials.gt_details}
-            />
-            <mesh
-              geometry={nodes.Punto_GT_8.geometry}
-              material={materials.gt_shadow}
-            />
-            <group rotation={[0.53, -0.23, 0.2]} />
-            <group rotation={[-1.55, 1.1, 0.63]} />
-          </group>
+    <group ref={group} {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <group position={[-0.04, 0.03, -0.04]} scale={0.46}>
+          <mesh
+            geometry={nodes.Punto_GT_0.geometry}
+            material={materials.gt_black}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_1.geometry}
+            material={materials.gt_license}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_2.geometry}
+            material={materials.gt_tire}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_3.geometry}
+            material={materials.gt_rim}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_4.geometry}
+            material={materials.gt_windows}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_5.geometry}
+            material={materials["Hemi.001"]}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_6.geometry}
+            material={materials.gt_body}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_7.geometry}
+            material={materials.gt_details}
+          />
+          <mesh
+            geometry={nodes.Punto_GT_8.geometry}
+            material={materials.gt_shadow}
+          />
+          <group rotation={[0.53, -0.23, 0.2]} />
+          <group rotation={[-1.55, 1.1, 0.63]} />
         </group>
       </group>
-    </Suspense>
+    </group>
   );
 };
 
